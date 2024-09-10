8d0de1c7e909c046829c8b22e44964976b0d9171
fangkeat <lfkisme@gmail.com>
2024-09-10 23:56:40 +0800

    Update ItemEntity
    Update ItemEntity

--- src/main/java/com/example/assessment/entity/ItemEntity.java
+++ src/main/java/com/example/assessment/entity/ItemEntity.java
@@ -19,10 +19,8 @@ public class ItemEntity {
     private Long id;
 
     @Column(length = 100)
-    @Size(min = 1, max = 100)
     private String name;
 
-    @Column(length = 100)
-    @Size(max = 255)
+    @Column(length = 255)
     private String description;
 }
